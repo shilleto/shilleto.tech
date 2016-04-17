adc161f248a3c3a4a9e245104437cca3cc610962
Luke Shilleto <luke@shilleto.tech>
2016-04-17 18:35:25 +0100

    Minify js

--- site/javascripts/cbpAnimatedHeader.min.min.js
+++
@@ -1,1 +0,0 @@
-var cbpAnimatedHeader=function(){function e(){window.addEventListener("scroll",function(e){a||(a=!0,setTimeout(n,250))},!1)}function n(){var e=c();e>=o?classie.add(t,"cbp-af-header-shrink"):classie.remove(t,"cbp-af-header-shrink"),a=!1}function c(){return window.pageYOffset||r.scrollTop}var r=document.documentElement,t=document.querySelector(".cbp-af-header"),a=!1,o=300;e()}();
